9f12203c07c73739be105234c256b0d9c04e9d20
amberyangYH <amber.yh.yang@gmail.com>
2018-01-10 17:15:26 +0800

    Photo Take/Pick

--- app/src/main/java/com/project/pp/parentparadise/amber/ShareAddArticle.java
+++ app/src/main/java/com/project/pp/parentparadise/amber/ShareAddArticle.java
@@ -5,34 +5,27 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Point;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -41,11 +34,8 @@ import com.google.zxing.integration.android.IntentResult;
 import com.project.pp.parentparadise.R;
 import com.project.pp.parentparadise.ppmain.Member;
 import com.project.pp.parentparadise.utl.SessionManager;
-import com.zfdang.multiple_images_selector.ImagesSelectorActivity;
-import com.zfdang.multiple_images_selector.SelectorSettings;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,16 +47,17 @@ import java.util.List;
 public class ShareAddArticle extends AppCompatActivity {
     private static final String TAG = "ShareAddArticle";
     private Spinner spClassify1;
-    private Button btScan, btPhoto;
+    private Button btScan, btTakePhoto, btPickPhoto;
     private EditText etAddIsbn, etDescribe;
     private String shareType;
-    private LinearLayout llActMain, llBookData;
-    private ScrollView svAddArticle;
-    private GridView gvPhotoPicked;
+    private LinearLayout llActMain, llBookData, llAddArticle;
+    private RecyclerView rvPhotoPicked;
     private ArrayList<String> pathList = new ArrayList<>();
-    private static final int REQUEST_PICTURE = 0;
+    private static final int REQUEST_TAKE_PICTURE = 0;
+    private static final int REQUEST_PICK_PICTURE = 1;
     private ShareTask addArticleTask, upLoadPhotoTask;
-    int newSize = 512;
+    private File file;
+    private int newSize = 512;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,22 +71,22 @@ public class ShareAddArticle extends AppCompatActivity {
     private void findviews() {
         spClassify1 = findViewById(R.id.spClassify1);
         btScan = findViewById(R.id.btScan);
-        btPhoto = findViewById(R.id.btPhoto);
+        btTakePhoto = findViewById(R.id.btTakePhoto);
+        btPickPhoto = findViewById(R.id.btPickPhoto);
         etAddIsbn = findViewById(R.id.etAddIsbn);
         etDescribe = findViewById(R.id.etDescribe);
         llActMain = findViewById(R.id.llActMain);
         llBookData = findViewById(R.id.llBookData);
-        svAddArticle = findViewById(R.id.svAddArticle);
-        gvPhotoPicked = findViewById(R.id.gvPhotoPicked);
+        llAddArticle = findViewById(R.id.llAddArticle);
+        rvPhotoPicked = findViewById(R.id.rvPhotoPicked);
         spClassify1.setSelection(0, true);
         spClassify1.setOnItemSelectedListener(spListener1);
     }
 
     private void initContent() {
         llBookData.setVisibility(View.GONE);
-        svAddArticle.setVisibility(View.GONE);
+        llAddArticle.setVisibility(View.GONE);
     }
-
 
     @Override
     protected void onStart() {
@@ -103,7 +94,6 @@ public class ShareAddArticle extends AppCompatActivity {
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
         ShareCommon.askPermissions(this, permissions, ShareCommon.PERMISSION_READ_EXTERNAL_STORAGE);
     }
-
     Spinner.OnItemSelectedListener spListener1 = new Spinner.OnItemSelectedListener() {
 
         @Override
@@ -114,29 +104,29 @@ public class ShareAddArticle extends AppCompatActivity {
                     break;
                 case 1:
                     initContent();
-                    svAddArticle.setVisibility(View.VISIBLE);
+                    llAddArticle.setVisibility(View.VISIBLE);
                     shareType = "S";
                     break;
                 case 2:
                     initContent();
-                    svAddArticle.setVisibility(View.VISIBLE);
+                    llAddArticle.setVisibility(View.VISIBLE);
                     shareType = "A";
                     break;
                 case 3:
                     initContent();
                     llBookData.setVisibility(View.VISIBLE);
                     btScan.setVisibility(View.VISIBLE);
-                    svAddArticle.setVisibility(View.VISIBLE);
+                    llAddArticle.setVisibility(View.VISIBLE);
                     shareType = "B";
                     break;
                 case 4:
                     initContent();
-                    svAddArticle.setVisibility(View.VISIBLE);
+                    llAddArticle.setVisibility(View.VISIBLE);
                     shareType = "M";
                     break;
                 case 5:
                     initContent();
-                    svAddArticle.setVisibility(View.VISIBLE);
+                    llAddArticle.setVisibility(View.VISIBLE);
                     shareType = "L";
                     break;
                 default:
@@ -149,23 +139,6 @@ public class ShareAddArticle extends AppCompatActivity {
         }
     };
 
-    //檢查如果user不允許使用存取外部儲存體，讓按鈕失效
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case ShareCommon.PERMISSION_READ_EXTERNAL_STORAGE:
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    btPhoto.setEnabled(true);
-                } else {
-                    btPhoto.setEnabled(false);
-                }
-                break;
-        }
-    }
-
     public void OnScanClick(View view) {
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setBarcodeImageEnabled(true);
@@ -176,80 +149,87 @@ public class ShareAddArticle extends AppCompatActivity {
         integrator.initiateScan();
     }
 
-    public void OnPhotoClick(View view) {
-        // start multiple photos selector
-        Intent intent = new Intent(ShareAddArticle.this, ImagesSelectorActivity.class);
-        // max number of images to be selected
-        intent.putExtra(SelectorSettings.SELECTOR_MAX_IMAGE_NUMBER, 12);
-        // min size of image which will be shown; to filter tiny images (mainly icons)
-        intent.putExtra(SelectorSettings.SELECTOR_MIN_IMAGE_SIZE, 100000);
-        // show camera or not
-        intent.putExtra(SelectorSettings.SELECTOR_SHOW_CAMERA, true);
-        // pass current selected images as the initial value
-        intent.putStringArrayListExtra(SelectorSettings.SELECTOR_INITIAL_SELECTED_LIST, pathList);
-        // start the selector
-        startActivityForResult(intent, REQUEST_PICTURE);
-    }
-
-    public class PhotoAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return pathList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return pathList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View itemView, ViewGroup parent) {
-            if (itemView == null) {
-                LayoutInflater layoutInflater = LayoutInflater.from(ShareAddArticle.this);
-                itemView = layoutInflater.inflate(R.layout.share_add_article_itemview, parent, false);
-            }
-
-            ImageView ivPhoto = itemView.findViewById(R.id.ivPhoto);
-//            ivPhoto.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
+//    public void OnTackPhotoClick(View view) {
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        file = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        file = new File(file, "picture.jpg");
 //
-//                }
-//            });
+//        Uri contentUri = FileProvider.getUriForFile(
+//                this, getPackageName() + ".provider", file);
+//        String path = contentUri.toString();
+//        pathList.add(path);
+//
+//        intent.putStringArrayListExtra(MediaStore.EXTRA_OUTPUT , pathList);
+//        Log.d(TAG, intent.toString());
+//        if (isIntentAvailable(this, intent)) {
+//            startActivityForResult(intent, REQUEST_TAKE_PICTURE);
+//        } else {
+//            Toast.makeText(this, "No Camera Apps Found",
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
-            ivPhoto.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            Bitmap srcImage = BitmapFactory.decodeFile(pathList.get(position));
-            Bitmap downsizedImage = ShareCommon.downSize(srcImage, newSize);
-            ivPhoto.setImageBitmap(downsizedImage);
+//    public void OnPickPhotoClick(View view) {
+//        Intent intent = new Intent(Intent.ACTION_PICK,
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        Uri contentUri = FileProvider.getUriForFile(
+//                this, getPackageName() + ".provider", file);
+//        String path = contentUri.toString();
+//        pathList.add(path);
+//        intent.putStringArrayListExtra(MediaStore.EXTRA_OUTPUT, pathList);
+//
+//        startActivityForResult(intent, REQUEST_PICK_PICTURE);
+//    }
 
-            return itemView;
+    //    檢查可拍照的軟體，可能有多個(回傳List)
+    public boolean isIntentAvailable(Context context, Intent intent) {
+        PackageManager packageManager = context.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    //    檢查如果user不允許使用存取外部儲存體，讓按鈕失效
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case ShareCommon.PERMISSION_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    btTakePhoto.setEnabled(true);
+                    btPickPhoto.setEnabled(true);
+                } else {
+                    btTakePhoto.setEnabled(false);
+                    btPickPhoto.setEnabled(false);
+                }
+                break;
         }
     }
+
 
     // 傳資料到畫面
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         Log.d("Received", "onActivityResult");
+        rvPhotoPicked.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL));
+        PagerSnapHelper snapHelper = new PagerSnapHelper();
         if (resultCode == RESULT_OK) {
-            int newSize = 512;
             switch (requestCode) {
-                case REQUEST_PICTURE:
-                    pathList = intent.getStringArrayListExtra(SelectorSettings.SELECTOR_RESULTS);
-                    assert pathList != null;
+                case REQUEST_TAKE_PICTURE:
+                    pathList = intent.getStringArrayListExtra(MediaStore.ACTION_IMAGE_CAPTURE);
+                    rvPhotoPicked.setAdapter(new PhotoPathAdapter(this, pathList));
+                    rvPhotoPicked.setOnFlingListener(null);
+                    snapHelper.attachToRecyclerView(rvPhotoPicked);
+                    break;
 
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(String.format("Totally %d images selected:", pathList.size())).append("\n");
-                    for (String result : pathList) {
-                        sb.append(result).append("\n");
-                    }
-                    gvPhotoPicked.setAdapter(new PhotoAdapter());
+                case REQUEST_PICK_PICTURE:
+                    pathList = intent.getStringArrayListExtra(MediaStore.ACTION_IMAGE_CAPTURE);
+                    rvPhotoPicked.setAdapter(new PhotoPathAdapter(this, pathList));
+                    rvPhotoPicked.setOnFlingListener(null);
+                    snapHelper.attachToRecyclerView(rvPhotoPicked);
                     break;
 
                 default:
@@ -265,12 +245,72 @@ public class ShareAddArticle extends AppCompatActivity {
         }
     }
 
-//UpLoadData to SQL
-    public void onSubmitClick(View view) {
-        ShareData shareData = null;
 
-        if (etDescribe.getText() == null && gvPhotoPicked.getCount() == 0) {
-            ShareCommon.showToast(this, R.string.msg_NotUploadWithoutPicture);
+    class PhotoPathAdapter extends RecyclerView.Adapter<PhotoPathAdapter.MyViewHolder> {
+        private Context context;
+        private ArrayList<String> pathList;
+
+        PhotoPathAdapter(Context context, ArrayList<String> pathList) {
+            this.context = context;
+            this.pathList = pathList;
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            ImageView ivPhoto;
+            LinearLayout llViewGroup;
+
+            MyViewHolder(View itemView) {
+                super(itemView);
+                ivPhoto = itemView.findViewById(R.id.ivPhoto);
+                llViewGroup = itemView.findViewById(R.id.llViewGroup);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return pathList.size();
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View itemView = layoutInflater.inflate(R.layout.share_add_article_itemview, viewGroup, false);
+            return new PhotoPathAdapter.MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+//            holder.ivPhoto.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            Bitmap srcImage = BitmapFactory.decodeFile(pathList.get(position));
+            Bitmap downsizedImage = ShareCommon.downSize(srcImage, newSize);
+            holder.ivPhoto.setImageBitmap(downsizedImage);
+
+            ImageView[] dots = new ImageView[pathList.size()];
+            for (int i = 0; i < pathList.size(); i++) {
+                ImageView mImageView = new ImageView(ShareAddArticle.this);
+                dots[i] = mImageView;
+                LinearLayout.LayoutParams ll = new LinearLayout.LayoutParams
+                        (new ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT));
+                ll.rightMargin = 6;
+                ll.leftMargin = 6;
+                ll.bottomMargin = 8;
+
+                if (i == position) {
+                    mImageView.setBackgroundResource(R.drawable.amber_icon_dot_yellow);
+                } else {
+                    mImageView.setBackgroundResource(R.drawable.amber_icon_dot_white);
+                }
+                holder.llViewGroup.addView(mImageView, ll);
+            }
+        }
+    }
+    //UpLoadData to SQL
+    public void onSubmitClick(View view) {
+
+        if (etDescribe.getText() == null && pathList.size() == 0) {
+            ShareCommon.showToast(this, R.string.msg_NoUploadMessage);
+            return;
         } else {
             SessionManager session = new SessionManager(getApplicationContext());
             Member loginMember = session.getLoginMemberInfo();
@@ -282,9 +322,8 @@ public class ShareAddArticle extends AppCompatActivity {
 
             int shareId = 0;
             int photoCount = 0;
-
             if (ShareCommon.networkConnected(this)) {
-                shareData = new ShareData(member_no, shareType, shareContent, isbn);
+                ShareData shareData = new ShareData(member_no, shareType, shareContent, isbn);
                 JsonObject jsonDataObject = new JsonObject();
                 jsonDataObject.addProperty("action", "addArticle");
                 jsonDataObject.addProperty("shareData", new Gson().toJson(shareData));
@@ -297,11 +336,6 @@ public class ShareAddArticle extends AppCompatActivity {
                     Log.e(TAG, e.toString());
                 }
 
-            } else {
-                ShareCommon.showToast(this, R.string.msg_NoNetwork);
-            }
-
-            if (ShareCommon.networkConnected(this)) {
                 if (pathList.size() > 0) {
                     for (int i = 0; i < pathList.size(); i++) {
 
@@ -317,18 +351,24 @@ public class ShareAddArticle extends AppCompatActivity {
 
                         upLoadPhotoTask = new ShareTask(url, jsonImageObject.toString());
                         try {
-                            upLoadPhotoTask.execute();
-//                            photoCount = Integer.valueOf(data);
+                            String result = upLoadPhotoTask.execute().get();
+                            photoCount = Integer.valueOf(result);
                         } catch (Exception e) {
                             Log.e(TAG, e.toString());
                         }
                     }
                 }
-//                if (etDescribe.getText() != null && shareId !=0 || gvPhotoPicked.getCount() != 0 && photoCount!=0) {
-//                    ShareCommon.showToast(this, R.string.msg_InsertFail);
-//                } else {
-//                    ShareCommon.showToast(this, R.string.msg_InsertSuccess);
-//                }
+
+                if (etDescribe.getText() == null && pathList.size() == 0) {
+                    ShareCommon.showToast(this, R.string.msg_InsertFail);
+//                    finish();
+                } else {
+                    ShareCommon.showToast(this, R.string.msg_InsertSuccess);
+                    finish();
+                    Intent intent = new Intent(this, ShareFragmentSweet.class);
+                    intent.putExtra("id",1);
+                    startActivity(intent);
+                }
 
             } else {
                 ShareCommon.showToast(this, R.string.msg_NoNetwork);
@@ -343,5 +383,6 @@ public class ShareAddArticle extends AppCompatActivity {
             addArticleTask.cancel(true);
         }
     }
+
 }
 
